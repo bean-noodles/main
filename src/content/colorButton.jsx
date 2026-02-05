41ccf3159e96e8ccbf1096ce53f0bf83f011640a
dodo07070707 <aa01056328583@gmail.com>
2026-02-06 00:15:46 +0900

    grey icon add

--- src/content/colorButton.jsx
+++ src/content/colorButton.jsx
@@ -3,11 +3,13 @@ import React, { useState } from "react";
 import redIcon from "../assets/icon/red.svg";
 import yellowIcon from "../assets/icon/yellow.svg";
 import greenIcon from "../assets/icon/green.svg";
+import greyIcon from "../assets/Icon/grey.svg";
 
 const STATUS = [
   { label: "안전", color: "green", icon: greenIcon },
   { label: "주의", color: "yellow", icon: yellowIcon },
   { label: "위험", color: "red", icon: redIcon },
+  { label: "검사 중", color: "grey", icon: greyIcon },
 ];
 
 export default function ColorButton({ title, link }) {
