7675d7f171d10ef1470e660ab652444c57e5d0e8
dodo07070707 <aa01056328583@gmail.com>
2026-02-06 22:59:27 +0900

    flex error

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,12 +1,18 @@
 import ReactDOM from "react-dom/client";
 import "./styles/popup.css";
 import headerIcon from "../public/popup-header.svg";
+import ProfileIcon from "../public/profile.svg";
+import SettingIcon from "../public/setting.svg";
 
 export default function Popup() {
   return (
     <div className="popup-container">
       <div className="popup-header-wrapper">
         <img src={headerIcon} className="popup-header-icon" />
+        <div className="popup-header-right-wrapper">
+          <img src={SettingIcon} className="popup-header-right-icon" />
+          <img src={ProfileIcon} className="popup-header-right-icon" />
+        </div>
       </div>
     </div>
   );
