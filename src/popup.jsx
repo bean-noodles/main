b9935305735bd79ed1842a70abd00a2f5b3fe104
dodo07070707 <aa01056328583@gmail.com>
2026-02-07 16:24:26 +0900

    end : settingpage

--- src/popup.jsx
+++ src/popup.jsx
@@ -7,6 +7,7 @@ import ColorButton from "./content/colorButton.jsx";
 import Arrowup from "../public/arrow_up.svg";
 import Arrowdown from "../public/arrow_down.svg";
 import Arrowback from "../public/arrow_back.svg";
+import Logout from "../public/logout.svg";
 
 function Popup() {
   const [results, setResults] = useState([]);
@@ -184,11 +185,70 @@ function Popup() {
     );
   };
 
+  const ProfilePage = () => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {/* 뒤로가기 */}
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            gap: 4,
+            alignItems: "center",
+            justifyContent: "start",
+            paddingBottom: 24,
+            paddingTop: 8,
+            cursor: "pointer",
+          }}
+          onClick={() => setPage("main")}
+        >
+          <img src={Arrowback} style={{ width: 16 }} />
+          <div style={{ fontWeight: 500, fontSize: 14, color: "black" }}>
+            뒤로 가기
+          </div>
+        </div>
+
+        <img
+          src={ProfileIcon}
+          style={{ width: 128, height: 128, marginBottom: 24 }}
+        ></img>
+        <div style={{ fontSize: 14, fontWeight: 500, marginBottom: 24 }}>
+          SYEONGMYING CHO
+        </div>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#000000",
+            width: "100%",
+            height: 40,
+            borderRadius: 8,
+          }}
+        >
+          <div style={{ display: "flex", flexDirection: "row", gap: 8 }}>
+            <img src={Logout} style={{ width: 16 }}></img>
+            <div style={{ color: "white", fontWeight: 500, fontSize: 14 }}>
+              로그아웃
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div
       style={{
         width: 300,
-        height: page === "settings" ? 250 : 400,
+        height: page === "settings" ? 250 : page === "profile" ? 350 : 400,
         fontFamily: "Pretendard Variable, sans-serif",
         backgroundColor: "white",
         color: "black",
@@ -209,11 +269,21 @@ function Popup() {
             style={{ width: 24, height: 24, cursor: "pointer" }}
             onClick={() => setPage("settings")}
           />
-          <img src={ProfileIcon} style={{ width: 24, height: 24 }} />
+          <img
+            src={ProfileIcon}
+            style={{ width: 24, height: 24, cursor: "pointer" }}
+            onClick={() => setPage("profile")}
+          />
         </div>
       </div>
 
-      {page === "main" ? <MainPage /> : <SettingsPage setPage={setPage} />}
+      {page === "main" ? (
+        <MainPage />
+      ) : page === "settings" ? (
+        <SettingsPage setPage={setPage} />
+      ) : (
+        <ProfilePage />
+      )}
     </div>
   );
 }
